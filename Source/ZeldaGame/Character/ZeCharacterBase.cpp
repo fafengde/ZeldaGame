55889e35d60571e57d0e39fb55f21c2f29db2a95
fafengde <1042427623@qq.com>
2026-07-14 14:54:39 +0800

    将UI界面基本彻底完善，增加了邮箱验证格式

---
+++ Source/ZeldaGame/Character/ZeCharacterBase.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZeCharacterBase.h"
+
+
+// Sets default values
+AZeCharacterBase::AZeCharacterBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AZeCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZeCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AZeCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
